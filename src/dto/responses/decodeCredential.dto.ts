8f42f6c8072b81cce73f959ffbffa836d79277cf
Cong Minh Do <meo123zzz@gmail.com>
2022-09-26 21:37:54 +0700

    fix bug filter teacher course and avatar user

--- src/dto/responses/decodeCredential.dto.ts
+++ src/dto/responses/decodeCredential.dto.ts
@@ -6,4 +6,5 @@ export default class DecodeCredentialDto {
     userName: string;
     role: AccountRole;
     exp: number;
+    avatar: string;
 }
--- src/init/InitData.ts
+++ src/init/InitData.ts
@@ -175,7 +175,7 @@ export async function initData() {
     const curriculumEnglish10 = await Curriculum.save(Curriculum.create({
         name: "Chương trình tiếng anh lớp 10",
         desc: "Khóa học Tiếng Anh 10 chương trình mới này bao gồm những bài học được sắp xếp một cách có hệ thống, logic thông qua những hoạt động đa dạng, những trò chơi thú vị để bạn học kiến thức, từ vựng một cách dễ dàng mà không cảm thấy nhàm chán. Chủ đề trong bài cũng vô cùng phong phú, ví dụ như về Đời sống gia đình, Âm nhạc, Những phát minh, Môi trường, Xã hội... bạn không chỉ được bổ sung về ngữ pháp mà còn được tích lũy thêm nhiều kiến thức ngoài xã hội nữa. Nhờ vậy, bạn sẽ nhanh chóng xây dựng được cho mình một nền tảng cơ bản để phát triển dần các kỹ năng ngôn ngữ.",
-        image: "Image",
+        image: "/assets/images/cirriculum/init_image.jpg",
     }));
 
     //Create Lectures of Curriculum
@@ -217,7 +217,7 @@ export async function initData() {
         price: 300000,
         openingDate: new Date(2022, 1, 1),
         closingDate: new Date(2022, 5, 31),
-        image: "Image",
+        image: "/assets/images/course/init_course.jpg",
         curriculum: curriculumEnglish10,
         teacher: teacherMinh,
     }));
@@ -230,7 +230,7 @@ export async function initData() {
         price: 300000,
         openingDate: new Date(2022, 1, 1),
         closingDate: new Date(2022, 5, 31),
-        image: "Image",
+        image: "/assets/images/course/init_course.jpg",
         curriculum: curriculumEnglish10,
         teacher: teacherMinh,
     }));
@@ -240,7 +240,7 @@ export async function initData() {
     const curriculumToeic550_650 = await Curriculum.save(Curriculum.create({
         name: "KHÓA HỌC TOEIC 550 - 650+",
         desc: "Lấy lại kiến thức căn bản tiếng Anh (Basic TOEIC) và 250 - 300 (Pre TOEIC), 350 - 400 (TOEIC A)",
-        image: "Image",
+        image: "/assets/images/cirriculum/init_image.jpg",
     }));
 
     //Create Lectures of Curriculum
@@ -276,7 +276,7 @@ export async function initData() {
         price: 1000000,
         openingDate: new Date(2021, 6, 1),
         closingDate: new Date(2022, 9, 30),
-        image: "Image",
+        image: "/assets/images/course/init_course.jpg",
         curriculum: curriculumToeic550_650,
         teacher: teacherMinh,
     }));
@@ -289,7 +289,7 @@ export async function initData() {
         price: 1100000,
         openingDate: new Date(2022, 2, 15),
         closingDate: new Date(2022, 5, 15),
-        image: "Image",
+        image: "/assets/images/course/init_course.jpg",
         curriculum: curriculumToeic550_650,
         teacher: teacherMinh,
     }));
