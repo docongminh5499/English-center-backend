c48c3d78fc81a0762a881e3c9aa36e1c96fc0d0d
DulioDoann <hoc.doanjos1206@hcmut.edu.vn>
2022-09-27 14:35:34 +0700

    implement register funcction

--- src/repositories/account/account.repository.interface.ts
+++ src/repositories/account/account.repository.interface.ts
@@ -1,5 +1,8 @@
 import { Account } from "../../entities/Account";
+import { User } from "../../entities/UserEntity";
 
 export default interface UserRepository {
     findByUserName: (username: string | undefined) => Promise<Account | null>;
+
+    findUserByEmail: (email: string | undefined) => Promise<User | null>;
 }
--- src/services/user/user.service.interface.ts
+++ src/services/user/user.service.interface.ts
@@ -1,8 +1,14 @@
 import { AccountDto, DecodeCredentialDto } from "../../dto";
 import { CredentialDto } from "../../dto";
+import { Account } from "../../entities/Account";
+import { User } from "../../entities/UserEntity";
 
 export default interface UserService {
     signin: (dto: AccountDto) => Promise<CredentialDto>;
 
     decode: (dto: CredentialDto) => Promise<DecodeCredentialDto>;
+
+    signup: (user: User, account: Account) => any;
+
+    checkOldEmail: (email: string) => Promise<Boolean>;
 }
