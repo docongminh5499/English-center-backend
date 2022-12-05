a0eb286a6df34336b7536cc0af194ca2207d21a5
Cong Minh Do <meo123zzz@gmail.com>
2022-12-05 09:56:38 +0700

    Change param name

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -8,7 +8,7 @@ import { DefaultEventsMap } from "socket.io/dist/typed-events";
 let io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
 export const socketInitialization = (server: http.Server) => {
   io = new Server(server, {
-    cors: { origin: process.env.API_HOST_URL }
+    cors: { origin: process.env.SOCKET_CORS_ACCEPT_URL }
   });
 
   addBasicEventHandler(io);
